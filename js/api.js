f6f8d7579ebe708d69a1644249701a6ec6bd47e4
Lucas Gomes <lulinux2109@gmail.com>
2025-07-10 01:40:21 -0300

    feat: cria a service para fazer as chamadas da api

---
+++ js/api.js
@@ -0,0 +1,36 @@
+const API_URL = "http://localhost/agenda-api/contatos";
+
+export const getContatos = async () => {
+  const res = await fetch(API_URL);
+  return res.json();
+};
+
+export const getContato = async (id) => {
+  const res = await fetch(`${API_URL}/${id}`);
+  return res.json();
+};
+
+export const createContato = async (data) => {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+};
+
+export const updateContato = async (id, data) => {
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+};
+
+export const deleteContato = async (id) => {
+  const res = await fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  });
+  return res.json();
+};
